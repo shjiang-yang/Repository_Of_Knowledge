6745445b7660ded8f5825f96161f5577ef6e9c38
shjiang-yang <49501818+shjiang-yang@users.noreply.github.com>
2020-06-08 14:40:49 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Repository_Of_Knowledge
- 知识点记录
+> 该仓库是学习过程中遇到的知识点  
+> 该README文件是这些知识点的索引表，并由Update_RMFile.py自动生成。  
+## FPGA  
+1. [JTAG.md](./FPGA/JTAG.md)  
+## Machine_Learning  
+1. [TransferLearning.md](./Machine_Learning/TransferLearning.md)  
---
+++ Update_RMFile.py
@@ -0,0 +1,29 @@
+#======================================
+#auther: shjiang
+#time: 20200608
+#description: 用于更新README文件
+#======================================
+
+import os
+
+
+FileHead = '''# Repository_Of_Knowledge
+> 该仓库是学习过程中遇到的知识点  
+> 该README文件是这些知识点的索引表，并由Update_RMFile.py自动生成。  
+'''
+
+
+def addIndex(writeInFile):
+    for _ in os.listdir("./"):
+        if '.' not in _:
+            writeInFile.write("## "+_+"  \n")
+            print("\033[1;31;40m \ncurrent folder: %s \033[0m" % (_))
+            count = 1
+            for f in os.listdir("./"+_+"/"):
+                writeInFile.write("%d. [%s](./%s/%s)  \n" % (count, f, _, f))
+                count += 1
+                print("add finished! -> %s" % (f))
+
+with open("README.md", "w", encoding="utf-8") as RMFile:
+    RMFile.write(FileHead)
+    addIndex(RMFile)
